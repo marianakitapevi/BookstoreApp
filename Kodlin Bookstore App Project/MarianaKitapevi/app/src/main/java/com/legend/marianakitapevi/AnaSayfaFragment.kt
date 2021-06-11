75e5f7c39980ca0a1dd6c12d5239b4e1546f7b20
Rumeysanegis <negisrumeysa@gmail.com>
2021-06-11 18:19:34 +0300

    Update AnaSayfaFragment.kt

--- Kodlin Bookstore App Project/MarianaKitapevi/app/src/main/java/com/legend/marianakitapevi/AnaSayfaFragment.kt
+++ Kodlin Bookstore App Project/MarianaKitapevi/app/src/main/java/com/legend/marianakitapevi/AnaSayfaFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+private val Button.setOnClick: Unit
+    get() {}
 
 
 class AnaSayfaFragment : Fragment() {
@@ -23,5 +28,62 @@ class AnaSayfaFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ana_sayfa, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        kitap_git.setOnClickListener() {
+            val aksiyon_kitap = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToKitaplarFragment()
+            Navigation.findNavController(it).navigate((aksiyon_kitap))
+        }
+
+        dergi_git.setOnClickListener() {
+            val aksiyon_dergi = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToDergilerFragment()
+            Navigation.findNavController(it).navigate((aksiyon_dergi))
+        }
+
+        blog_git.setOnClickListener() {
+            val aksiyon_blog = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToBlogFragment()
+            Navigation.findNavController(it).navigate((aksiyon_blog))
+        }
+
+        yazarlar_git.setOnClickListener() {
+            val aksiyon_yazar = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToYazarlarFragment()
+            Navigation.findNavController(it).navigate((aksiyon_yazar))
+        }
+
+        oneriler_git.setOnClickListener() {
+            val aksiyon_oneri = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToOnerilerFragment()
+            Navigation.findNavController(it).navigate((aksiyon_oneri))
+        }
+
+        ozet_git.setOnClickListener() {
+            val aksiyon_ozet = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToOzetlerFragment()
+            Navigation.findNavController(it).navigate((aksiyon_ozet))
+        }
+
+        sozler_git.setOnClickListener() {
+            val aksiyon_sozler = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToSozlerFragment()
+            Navigation.findNavController(it).navigate((aksiyon_sozler))
+                }
+
+        yayinevi_git.setOnClickListener() {
+            val aksiyon_yayinevi = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToYayinEviFragment()
+            Navigation.findNavController(it).navigate((aksiyon_yayinevi))
+        }
+
+        duyuru_git.setOnClickListener() {
+            val aksiyon_duyuru = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToDuyuruFragment()
+            Navigation.findNavController(it).navigate((aksiyon_duyuru))
+                }
+
+        notlar_git.setOnClickListener() {
+            val aksiyon_notlar = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToNotAlFragment()
+            Navigation.findNavController(it).navigate((aksiyon_notlar))
+        }
+
+        kategori_git.setOnClickListener() {
+            val aksiyon_kategori = AnaSayfaFragmentDirections.actionAnaSayfaFragmentToKategorilerFragment()
+            Navigation.findNavController(it).navigate((aksiyon_kategori))
+        }
+    }
 }
